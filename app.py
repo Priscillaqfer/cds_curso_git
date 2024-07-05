7b734aaaeb826d3d1b25b33e7e1d45d85e4471f6
Priscilla Ferreira <priscillaqf@hotmail.com>
2024-07-04 22:19:16 -0300

    Criando  arquivo de carregamento de dados e ajustado arquivo principal para utilizar esse novo arquivo

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.set_page_config(layout='wide')
 
 
 def  main():
-     df = load_data()
+     df_raw= load_data()
 
-     st.dataframe(df)
+     st.dataframe(df_raw)
 
 if __name__ == '__main__':
     main()
